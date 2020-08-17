938824d45be1b8aa8deac273179334ffd9449a53
Mustafa Ali <mali39@student.gsu.edu>
2020-08-17 09:11:53 -0400

    RGB Color Game using HTML, CSS, and JS

---
+++ ColorGame/index.js
@@ -0,0 +1,127 @@
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyBtn");
+var hardButton = document.querySelector("#hardBtn");
+
+easyButton.addEventListener("click", function() {
+    easyButton.classList.add("selected");
+    hardButton.classList.remove("selected");
+    colors = generateRandomColors(3);
+    messageDisplay.textContent = "";
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+        if (i > 2) {
+            squares[i].style.display = "none";
+        }
+    }
+});
+hardButton.addEventListener("click", function() {
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    colors = generateRandomColors(6);
+    messageDisplay.textContent = "";
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+     if (i > 2) {
+            squares[i].style.display = "block";
+        }
+    }
+});
+
+
+resetButton.addEventListener("click", function() {
+    if (easyButton.classList.toggle("selected")===false) {
+        easyButton.classList.add("selected");
+        colors = generateRandomColors(3);
+        pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < colors.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    messageDisplay.textContent = "";
+    h1.style.backgroundColor = "steelblue";
+    resetButton.textContent = "New Colors";
+
+    }
+    else {
+    messageDisplay.textContent = "";
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue";
+    resetButton.textContent = "New Colors";
+
+    }
+});
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+            h1.style.background = clickedColor;
+            resetButton.textContent = "Play Again?";
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//add num random colors to arr
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor() {
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
